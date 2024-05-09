a6b247ffffd0953d5ccda9efc16765cc689dd0e3
meeeeRang <dpfkddl8854@gmail.com>
2024-05-09 16:02:57 +0900

    2024.05.09

--- js/TextRotate.js
+++ js/TextRotate.js
@@ -19,3 +19,91 @@ var Emblem = {
   };
   
   Emblem.init('.emblem');
+
+  var Emblem1 = {
+    init: function(el, str) {
+      var element = document.querySelector(el);
+      var text = str ? str : element.innerHTML;
+      element.innerHTML = '';
+      for (var i = 0; i < text.length; i++) {
+        var letter = text[i];
+        var span = document.createElement('span');
+        var node = document.createTextNode(letter);
+        var r = (360/text.length)*(i);
+        var x = (Math.PI/text.length).toFixed(0) * (i);
+        var y = (Math.PI/text.length).toFixed(0) * (i);
+        span.appendChild(node);
+        span.style.webkitTransform = 'rotateZ('+r+'deg) translate3d('+x+'px,'+y+'px,0)';
+        span.style.transform = 'rotateZ('+r+'deg) translate3d('+x+'px,'+y+'px,0)';
+        element.appendChild(span);
+      }
+    }
+  };
+  
+  Emblem.init('.emblem1');
+
+  var Emblem2 = {
+    init: function(el, str) {
+      var element = document.querySelector(el);
+      var text = str ? str : element.innerHTML;
+      element.innerHTML = '';
+      for (var i = 0; i < text.length; i++) {
+        var letter = text[i];
+        var span = document.createElement('span');
+        var node = document.createTextNode(letter);
+        var r = (360/text.length)*(i);
+        var x = (Math.PI/text.length).toFixed(0) * (i);
+        var y = (Math.PI/text.length).toFixed(0) * (i);
+        span.appendChild(node);
+        span.style.webkitTransform = 'rotateZ('+r+'deg) translate3d('+x+'px,'+y+'px,0)';
+        span.style.transform = 'rotateZ('+r+'deg) translate3d('+x+'px,'+y+'px,0)';
+        element.appendChild(span);
+      }
+    }
+  };
+  
+  Emblem.init('.emblem2');
+  
+  var Emblem3 = {
+    init: function(el, str) {
+      var element = document.querySelector(el);
+      var text = str ? str : element.innerHTML;
+      element.innerHTML = '';
+      for (var i = 0; i < text.length; i++) {
+        var letter = text[i];
+        var span = document.createElement('span');
+        var node = document.createTextNode(letter);
+        var r = (360/text.length)*(i);
+        var x = (Math.PI/text.length).toFixed(0) * (i);
+        var y = (Math.PI/text.length).toFixed(0) * (i);
+        span.appendChild(node);
+        span.style.webkitTransform = 'rotateZ('+r+'deg) translate3d('+x+'px,'+y+'px,0)';
+        span.style.transform = 'rotateZ('+r+'deg) translate3d('+x+'px,'+y+'px,0)';
+        element.appendChild(span);
+      }
+    }
+  };
+  
+  Emblem.init('.emblem3');
+  
+  var Emblem4 = {
+    init: function(el, str) {
+      var element = document.querySelector(el);
+      var text = str ? str : element.innerHTML;
+      element.innerHTML = '';
+      for (var i = 0; i < text.length; i++) {
+        var letter = text[i];
+        var span = document.createElement('span');
+        var node = document.createTextNode(letter);
+        var r = (360/text.length)*(i);
+        var x = (Math.PI/text.length).toFixed(0) * (i);
+        var y = (Math.PI/text.length).toFixed(0) * (i);
+        span.appendChild(node);
+        span.style.webkitTransform = 'rotateZ('+r+'deg) translate3d('+x+'px,'+y+'px,0)';
+        span.style.transform = 'rotateZ('+r+'deg) translate3d('+x+'px,'+y+'px,0)';
+        element.appendChild(span);
+      }
+    }
+  };
+  
+  Emblem.init('.emblem4');
--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,40 @@
 /*popup*/
 $(".emblem").click(function () {
-    $(".popupWrap").fadeIn(500);
+    $(".Wrap1").fadeIn(200);
 });
 $(".close").click(function () {
-    $(".popupWrap").fadeOut(200);
+    $(".Wrap1").fadeOut(400);
 });
+$(".emblem1").click(function () {
+    $(".Wrap2").fadeIn(200);
+});
+$(".close").click(function () {
+    $(".Wrap2").fadeOut(400);
+});
+$(".emblem2").click(function () {
+    $(".Wrap3").fadeIn(200);
+});
+$(".close").click(function () {
+    $(".Wrap3").fadeOut(400);
+});
+$(".emblem3").click(function () {
+    $(".Wrap4").fadeIn(200);
+});
+$(".close").click(function () {
+    $(".Wrap4").fadeOut(400);
+});
+$(".emblem4").click(function () {
+    $(".Wrap5").fadeIn(200);
+});
+$(".close").click(function () {
+    $(".Wrap5").fadeOut(400);
+});
+// function openModal(modalname){
+//     $("#popupWrap").fadeIn(300);
+//     $("."+modalname).fadeIn(300);
+//   }
+  
+//   $("#popupWrap, .close").on('click',function(){
+//     $("#popupWrap").fadeOut(300);
+//     $(".modal-con").fadeOut(300);
+//   });
